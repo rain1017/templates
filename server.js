463179250ad1777bbd390d379032547bc511fd14
Yu Xia <rain1017@gmail.com>
2015-07-22 16:28:44 +0800

    update template

--- server.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var net = require('net');
-
-var server = net.createServer(function (socket) {
-  socket.write('Echo server\r\n');
-  socket.pipe(socket);
-});
-
-server.listen(3000, '127.0.0.1');
